5777cf0d9c1553579edaf547fbf253d5c4a4032e
sandeep <chavda2991sandeep@gmail.com>
2023-07-04 07:31:22 +0530

    add provider in config file

--- config/app.php
+++ config/app.php
@@ -169,7 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
-        Spatie\Permission\PermissionServiceProvider::class
+        Spatie\Permission\PermissionServiceProvider::class  
     ])->toArray(),
 
     /*
